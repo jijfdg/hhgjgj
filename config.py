cfaa514eb49a2a2ff07b2d96db9915c19c5c0663
jijfdg <vokorobka@gmail.com>
2025-07-25 00:41:41 +0300

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Токен Telegram-бота
-TELEGRAM_TOKEN = "ВАШ_ТОКЕН_ТУТ"
+TELEGRAM_TOKEN = "8430063928:AAFidIFAxHZ9zxOpB6I6xHU99-Zt1YZ6zz4"
 
 # Ключ API Itphone
 ITPHONE_API_KEY = "ВАШ_API_КЛЮЧ_ITPHONE"
